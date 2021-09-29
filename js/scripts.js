008d6550953b1def47cdb5e9632449f85b4eb897
MaximSelivanov <max_sell@mail.ru>
2021-09-29 14:10:25 +0300

    service block  and slider

--- js/scripts.js
+++ js/scripts.js
@@ -6,5 +6,17 @@ $(function(){
         else {
             $('nav ul').slideUp()
         };
-    })
+    });
+    $('.service-slider').slick({
+		arrows: false,
+		dots: true,
+		responsive: [{
+			breakpoint: 600,
+			settings: {
+				dots: false,
+				centerMode: true,
+				centerPadding: '40px',
+			} 
+		}]
+	});
 })
